947b2324200c3722d25ce4743f3205dd0385f343
eligifty-coder <gift.weike@gmail.com>
2023-07-18 14:39:24 -0700

    betty error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
-/**
- * File: 1-alphabet.c
- * Return: Nothing.
- */
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
  */
 void print_alphabet(void)
 {
-	char alph;
+	char c;
 
-	for (alph = 'a'; alph <= 'z'; alph++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(alph);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
