3502c5ac3935f5cc71ef71acefb0db69b7ac8258
eligifty-coder <gift.weike@gmail.com>
2023-07-18 14:26:18 -0700

    I had issue with betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * File: 1-alphabet.c
  * main - Prints the alphabet, in lowercase, followed by a new line.
  * Return: 0 on success.
  */
+
 void print_alphabet(void)
 {
 	int alphabet;
-	for(alphabet = 'a'; alphabet<= 'z'; alphabet++)
+	for( alphabet = 'a'; alphabet <= 'z'; alphabet++ )
 	{
 		_putchar(alphabet);
 	}
