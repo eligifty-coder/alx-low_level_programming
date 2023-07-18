26603f39dc74cb64f8d2ca75b537bd20255a98ee
eligifty-coder <gift.weike@gmail.com>
2023-07-18 13:27:03 -0700

    error fixing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,13 @@
 /**
  * main = Entry point
  * Description: 'prints _putchar to stdout'
- * return : always 0
- * */
+ * Return : 0 on success.
+ */
 int main(void)
 {
 	char c[] = "_putchar";
 	int i;
-	for(i=0; i<9;i++)
+	for(i = 0; i < 9; i++)
 	{
 		_putchar(c[i]);
 	}
