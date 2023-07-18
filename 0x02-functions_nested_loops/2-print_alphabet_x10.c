b020012e4aaaa75f1f52624800829ea088d80a7b
eligifty-coder <gift.weike@gmail.com>
2023-07-18 16:18:50 -0700

    missing blank line after declaration

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,17 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
-
 void print_alphabet_x10(void)
 {
-	char count;
+	count = 0;
 
 	while (count < 10)
 	{
-		char ch;
-		for (ch = 'a'; ch <= 'z'; ch++)
+		char ch = 'a';
+		while (ch <= 'z')
 		{
 			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
 		count++;
