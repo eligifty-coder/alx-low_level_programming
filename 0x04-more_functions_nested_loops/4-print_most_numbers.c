68bb77003540fd091e8eaa5da03f862631967373
eligifty-coder <gift.weike@gmail.com>
2023-07-20 15:46:00 -0700

    error

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
-*print_most_numbers - prints
-* owned by Bwave ICT
-*Return: void
-*/
+ * print_most_numbers - check the code for Holberton School students.
+ * Return: Always 0.
+ */
 
 void print_most_numbers(void)
 {
-	char c;
+	char c = '0';
 
-	for (c = '0'; c <= '9'; c++)
+	while (c <= '9')
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+	c++;
 	}
-	_putchar('\n');
+_putchar('\n');
 }
