a9f0e98b4a79434a2b79ad82ff4ebbb1c1479825
eligifty-coder <gift.weike@gmail.com>
2023-07-20 17:13:42 -0700

    Write a function that prints 10 times the numbers, from 0 to 14, followed by a new line.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,2 +1,33 @@
-nclude "main.h"
+include "main.h"
+
+/**
+ * more_numbers - Write a function that prints 10 times the numbers, from 0 to 14, 
+ * followed by a new line.
+ * Return: void
+ */ 
+
+void more_numbers(void)
+{
+	int i = 0; 
+	int j;
+
+	while(i <= 9)
+	{
+		j = 0;
+
+		while(j <= 14)
+		{
+			if(j > 9)
+			{
+				_putchar(j / 10 + '0');
+			}
+
+			_putchar(j % 10 + '0');
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
 
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -27,3 +27,6 @@ A function that prints the numbers, from 0 to 9, followed by a new line.
 
 <h6>4-print_most_numbers.c</h6>
 a function that prints the numbers, from 0 to 9, followed by a new line.
+
+<h6>5-more_numbers.c</h6>
+A function that prints 10 times the numbers, from 0 to 14, followed by a new line.
