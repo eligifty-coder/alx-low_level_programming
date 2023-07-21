4e2d702dc5a5a705c13b825fd2312c6920300af9
eligifty-coder <gift.weike@gmail.com>
2023-07-20 17:39:52 -0700

    betty error

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * print_line - function that draws a straight line in the terminal.
- * The line should end with a \n
- * @n: parameter to the print_line function
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-	int i = 0;
-
-	if(n > 0)
+	if (n <= 0)
 	{
-		while(i <= n)
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
-			i++;
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 
+}
