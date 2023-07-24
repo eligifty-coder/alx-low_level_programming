1af1f3f26373135291185d787c8603de703fcff7
eligifty-coder <gift.weike@gmail.com>
2023-07-24 13:57:46 -0700

    betty error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,28 @@
 #include "main.h"
-/**
- * print_rev - prints a string in reverse
- * followed by a new line.
- * @s: input string to print.
- */
-void print_rev(char *s)
-{
-	int i;
 
-	while (s[i] != '\0')
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		i++;
+		len++;
 	}
-	i--;
-	while (s[i] != '\0')
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
-		_putchar(s[i--]);
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
-	_putchar('\n');
 }
