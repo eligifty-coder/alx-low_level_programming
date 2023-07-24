ce7e3c2e011e339642a29e5e1af964953dea4c95
eligifty-coder <gift.weike@gmail.com>
2023-07-24 09:56:11 -0700

    betty complains

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
 /**
- * int _strlen - A function that returns the length of a string.
+ * _strlen - A function that returns the length of a string.
  * @s: pointer to the array of characters.
  * Return: The lenght of the string
  */
@@ -7,11 +7,11 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while(*s != '\0')
 	{
 		i++;
 		s++;
-	}
 
 	return (i);
 }
