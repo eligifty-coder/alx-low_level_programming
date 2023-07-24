958a00bef484345bb6eba5708656e2b75bfb10a2
eligifty-coder <gift.weike@gmail.com>
2023-07-24 15:10:13 -0700

    fixing error

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - A function that prints every other character of a string, starting with the first character,
+ * puts2 - A function that prints even numbers,
  * followed by a new line.
  * @str: string to print the chars from
  */
@@ -10,10 +10,12 @@ void puts2(char *str)
 {
 	int i;
 
-	while (str[i] != '\0')
+	while (*str[i] != '\0')
 	{
 		if (i % 2 == 0)
+		{
 			_putchar(str[i]);
+		}
 		i++;
 	}
 	_putchar('\n');
