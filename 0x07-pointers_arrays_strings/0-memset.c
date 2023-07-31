0cda660e2a0a8661f4544879ad8ee74ecb250ee2
eligifty-coder <gift.weike@gmail.com>
2023-07-31 15:59:46 -0700

    error in 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer to memory area 's'
@@ -8,6 +10,8 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
+
 	for(int i = 0; i < n; i++)
 	{
 		s[i] = b;
